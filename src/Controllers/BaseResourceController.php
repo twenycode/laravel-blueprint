26dcd853ba499141cb2cad769925fc23a2b90f79
TWENY <it@tweny.co.tz>
2025-04-14 10:51:21 +0300

    Update BaseResourceController.php

--- src/Controllers/BaseResourceController.php
+++ src/Controllers/BaseResourceController.php
@@ -6,6 +6,7 @@ use TwenyCode\LaravelBlueprint\Traits\ErrorHandlerTrait;
 use TwenyCode\LaravelBlueprint\Helpers\TextHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use App\Http\Controllers\Controller;
 
 /**
  * Base Resource Controller with standard CRUD operations
